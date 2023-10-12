d08d7b23fa86dec5fc014c6c9b99713bb685de1f
Samyak Jain <147701243+samyakjain2431@users.noreply.github.com>
2023-10-12 17:14:29 +0530

    Delete STL directory

--- STL/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-void printVec(){
-        cout<<"the size of vector"<< sam.size()<<endl;
-        for (int i = 0; i < sam.size(); i++)
-        {
-            cout<<sam[i]<<endl;
-        }
-        cout<<endl;
-        
-    }
