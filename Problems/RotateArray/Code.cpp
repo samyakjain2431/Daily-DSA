a9cc75732f7639874be5c2856b2e16d3ba422a29
Samyak Jain <samyak.jn2431@gmail.com>
2023-10-19 23:56:13 +0530

    try 1

---
+++ Problems/RotateArray/Code.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int rotateArray(vector<int> &arr, int k){
+    cout<<"hellow"<<endl;
+    vector<int> temp;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        temp[(i+k)%arr.size()] = arr[i];    
+    }
+    cout<<"hellow"<<endl;
+
+    return 0;  
+}
+
+int main(){
+    
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    rotateArray(arr, 2);
+    // cout<<temp[0];
+    return 0;
+}
