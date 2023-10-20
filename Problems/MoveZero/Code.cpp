3e08a0aa2269e91c01a63709a45037bf2528a8e9
Samyak Jain <samyak.jn2431@gmail.com>
2023-10-20 13:54:19 +0530

    Moving Zeroes

---
+++ Problems/MoveZero/Code.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void displayArray(vector<int> &arr){
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
+}
+
+void moveZero(vector<int> &arr){
+    int lastZero;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(arr[i]==0){
+            lastZero = i;
+            break;
+        }
+    }
+    for (int i = lastZero+1; i < arr.size(); i++)
+    {
+        if(arr[i]!= 0){
+            int temp = arr[i];
+            arr[i] = arr[lastZero];
+            arr[lastZero] = temp;
+            lastZero++;
+        }
+    }
+}
+
+int main(){
+    
+    vector<int> arr = {0, 0, 1,0, 0, 3, 5, 0, 2, 0, 1,1,0};
+    cout<<"Before Operation : ";
+    displayArray(arr);
+    moveZero(arr);
+    cout<<"After Operation : ";
+    displayArray(arr);
+    
+    return 0;
+}
