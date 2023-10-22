9be13b00b0793b6b009edbc1307c0a79b14ac6df
Samyak Jain <samyak.jn2431@gmail.com>
2023-10-22 15:19:51 +0530

    Char array

---
+++ Problems/CheckIfSortedArray/Code.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    
+    vector<int> arr1 = {1,2,3,4,5};
+    vector<int> arr2 = {3,4,5, 1, 2};
+    vector<int> arr3 = {1,1,1,1,1};
+    vector<int> arr4 = {2, 3,1,4, 1};
+    
+    return 0;
+}
