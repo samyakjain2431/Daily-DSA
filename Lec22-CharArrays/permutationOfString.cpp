01d2f4e91a01146cd092331e71362a2936e48332
Samyak Jain <samyak.jn2431@gmail.com>
2024-02-07 23:26:12 +0530

    permutation of the string

--- Lec22-CharArrays/permutationOfString.cpp
+++ Lec22-CharArrays/permutationOfString.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int k = 0;
-bool checkPerm(int a1[26], int b1[26]){
+bool checkPerm(int a1[26], int b1[26], int len){
     k++;
         for(int i = 0; i<26; i++){
             if(a1[i] != b1[i])return false;
         }
-        cout<<"at shift "<<k<<endl;
+        cout<<"string matched from index "<<k-1<<" upto "<< k-2+len<<endl;
         return true;
     }
 
@@ -17,36 +17,36 @@ bool permutationOfString(string s1, string sub){
     int i = 0; int countSub[26] = {0}, countMain[26] = {0};
     for (int i = 0; i < sub.length(); i++)
     {
-        cout<<"enter 1st";
+        // cout<<"enter 1st";
         int index = sub[i] -'a';
         countSub[index]++;
-        cout<<"exit 1st";
-        cout<<endl;
+        // cout<<"exit 1st";
+        // cout<<endl;
     }
     
     int windowSize = sub.length();
     //for first window
     while (i < windowSize && i<s1.length())
     {
-        cout<<"enter 2";
+        // cout<<"enter 2";
         countMain[s1[i]-'a']++;
         i++;
-        cout<<"exit 2"<<endl;
+        // cout<<"exit 2"<<endl;
     }
     for (int i = 0; i < 26; i++)
     {
-        cout<<countMain[i];
+        // cout<<countMain[i];
     }
-    cout<<endl;
+    // cout<<endl;
     for (int i = 0; i < 26; i++)
     {
-        cout<<countSub[i];
+        // cout<<countSub[i];
     }
-    cout<<endl;
+    // cout<<endl;
     
     //checking
-    if( checkPerm(countMain, countSub) ){
-        cout<<"checked"<<endl;
+    if( checkPerm(countMain, countSub, sub.length()) ){
+        // cout<<"checked"<<endl;
         return 1;
     }
     while (i<s1.length())
@@ -56,7 +56,7 @@ bool permutationOfString(string s1, string sub){
         int oldIndex = s1[i-windowSize] -'a';
         countMain[oldIndex]--;
         i++;
-        if(checkPerm(countMain, countSub)){return 1;}
+        if(checkPerm(countMain, countSub, sub.length())){return 1;}
     }
     
     
@@ -71,8 +71,14 @@ int main(){
     cin>>s1;
     cout<<"enter sub string : ";
     cin>>sub;
-    cout<<permutationOfString(s1, sub)<<endl;
+    permutationOfString(s1, sub);
 
     
     return 0;
 }
+
+
+//Notes :
+// 1. It only finds first occurance 
+// 2. It provides starting and ending indexes of occurance
+// 3. It works only for lowercase
